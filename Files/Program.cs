d6fca9d8b03808182afa1b044db6a50d032218de
ilya13-10 <zelenin.ilya2010@yandex.ru>
2020-03-25 16:37:16 +0300

    not first commit

---
+++ Files/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для регестрации нажмите 1, если у вас уже имеется аккаунт, нажите 2");
+            double vibor = double.Parse(Console.ReadLine());
+            if (vibor == 1)
+            {
+                Console.WriteLine("Введите имя: ");
+                string Nazvanie = Console.ReadLine();
+                File.Create("C:\\ПР_Раб\\" + Nazvanie + ".txt");
+                Console.WriteLine("Введите Пароль: ");
+                string user_password = Console.ReadLine();
+                FileStream file_1 = new FileStream("C:" + Nazvanie + ".txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(file_1);
+                writer.WriteLine(user_password);
+                writer.Close();
+                Console.WriteLine("Регистрация завершена");
+            }
+            if (vibor == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите имя: ");
+                string Nazvanie = Console.ReadLine();
+                FileStream file_2 = new FileStream("C:" + Nazvanie + ".txt", FileMode.Open);
+                StreamReader reader = new StreamReader(file_2);
+                string proverka = reader.ReadLine();
+                reader.Close();
+                Console.WriteLine("Введите пароль:");
+                string password = Console.ReadLine();
+                if (proverka == password)
+                {
+                    Console.WriteLine("Вы авторизировались, добро пожаловать");
+                }
+                else Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
